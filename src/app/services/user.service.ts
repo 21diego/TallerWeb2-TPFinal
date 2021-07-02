c5f1018c1cb234c670aff3e1d8267de169794771
21diego <morinigodiego21@gmail.com>
2021-07-02 19:01:07 -0300

    solucion al problema de CORS

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 //import { Observable } from "rxjs/Observable";
 
 @Injectable({
@@ -9,9 +10,9 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   login(user){
-    return this.http.post("http://localhost:3000/login", user);
+    return this.http.post("/api/login", user);
   }
-  register(user) {
-    return this.http.post("http://localhost:3000/register", user);
+  register(user): Observable<any> {
+    return this.http.post("/api/register", user);
   }
 }
