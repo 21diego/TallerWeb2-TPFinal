c842e1eccc48fa6ce20d9ad8320788904e816e1c
21diego <morinigodiego21@gmail.com>
2021-07-02 18:46:42 -0300

    agrego conexion de login y registro entre angular y node. Hay problemas de CORS

--- src/app/views/login-view/login-view.component.ts
+++ src/app/views/login-view/login-view.component.ts
@@ -1,11 +1,25 @@
 import { Component } from "@angular/core";
+import { UserService } from "../../services/user.service";
 
 @Component({
     selector: 'login-view',
-    templateUrl: './login-view.component.html'
+    templateUrl: './login-view.component.html',
+    styleUrls: [ './login-view.component.css']
 })
 
 
 export class LoginView{
+    email: string;
+    password: string;
+
     title = "Esta es la pagina de login"
+
+    constructor(public userService: UserService) {}
+
+    loguearse(){
+        const user = { email: this.email, password: this.password };
+        this.userService.login(user).subscribe( data => {
+            console.log(data)
+        })
+    }
 }
--- src/app/views/register-view/register-view.component.ts
+++ src/app/views/register-view/register-view.component.ts
@@ -1,10 +1,27 @@
 import { Component } from "@angular/core";
+import { UserService } from "../../services/user.service";
+
 
 @Component({
     selector: 'register-view',
-    templateUrl: './register-view.component.html'
+    templateUrl: './register-view.component.html',
+    styleUrls : [ './register-view.component.css']
 })
 
+
+
 export class RegisterView{
+    name: string;
+    email: string;
+    password: string;
     title = "Esta es la pagina de registro"
+
+    constructor(public userService: UserService) {}
+
+    registrarse(){
+        const user = { name: this.name, email: this.email, password: this.password };
+        this.userService.register(user).subscribe( data => {
+            console.log(data)
+        })
+    }
 }
